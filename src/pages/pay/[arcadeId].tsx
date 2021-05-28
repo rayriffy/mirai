f9b7704467f7565a9560f65c458d50c1460708d2
Phumrapee Limpianchop <git@rayriffy.com>
2021-05-28 20:08:47 +0900

    fix: json serializing

--- src/pages/pay/[arcadeId].tsx
+++ src/pages/pay/[arcadeId].tsx
@@ -4,6 +4,7 @@ import { GetServerSideProps, NextPage } from 'next'
 
 import { ArcadeWithId } from '../../core/@types/ArcadeWithId'
 import { BranchWithId } from '../../core/@types/BranchWithId'
+import { Branch } from '../../core/@types/firebase/Branch'
 
 interface Props {
   arcade: ArcadeWithId
@@ -36,9 +37,17 @@ export const getServerSideProps: GetServerSideProps<Props> = async ctx => {
       const branchDoc = await firebase.firestore().collection('branches').doc(arcade.data.branchId).get()
 
       if (branchDoc.exists) {
+        const branchData = branchDoc.data() as Branch
+
         const branch = {
           id: branchDoc.id,
-          data: branchDoc.data(),
+          data: {
+            ...branchData,
+            location: {
+              latitude: branchData.location.latitude,
+              longitude: branchData.location.longitude,
+            },
+          },
         } as BranchWithId
 
         return {
@@ -54,6 +63,7 @@ export const getServerSideProps: GetServerSideProps<Props> = async ctx => {
       throw 'not-found'
     }
   } catch (e) {
+    console.error(e)
     return {
       notFound: true,
     }
