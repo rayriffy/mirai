c5c2c193046c11dcff25761031d6a37dc9a7c9cc
Phumrapee Limpianchop <git@rayriffy.com>
2021-05-29 07:28:35 +0900

    chore: staged changes

---
+++ src/modules/dashboard/services/useArcade.ts
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react'
+
+import { collection, doc, getFirestore, getDoc } from '@firebase/firestore'
+import { createFirebaseInstance } from '../../../core/services/createFirebaseInstance'
+import { Arcade } from '../../../core/@types/firebase/Arcade'
+
+export const useArcade = (arcadeId: string) => {
+  const [data, setData] = useState<Arcade>(null)
+  const [loading, setLoading] = useState<boolean>(true)
+  const [error, setError] = useState<boolean>(false)
+
+  useEffect(() => {
+    getDoc(doc(collection(getFirestore(createFirebaseInstance()), 'arcades'), arcadeId)).then(doc => {
+      if (doc.exists()) {
+        setData(doc.data() as Arcade)
+        setError(false)
+      } else {
+        setError(true)
+      }
+    }).catch(() => {
+      setError(true)
+    }).finally(() => {
+      setLoading(false)
+    })
+  }, [])
+
+  return {
+    data,
+    loading,
+    error,
+  }
+}
