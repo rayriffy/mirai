1a82a7310357923dd76cef235ddf71ad6c6f50db
Phumrapee Limpianchop <git@rayriffy.com>
2021-05-28 17:48:21 +0900

    chore: staged changes

--- src/app/components/layout.tsx
+++ src/app/components/layout.tsx
@@ -1,9 +1,34 @@
-import { FunctionComponent, Fragment } from 'react'
+import { useRouter } from 'next/router'
+import { FunctionComponent, Fragment, useEffect } from 'react'
+import { useStoreon } from '../../context/storeon'
+import { useAuth } from '../../core/services/useAuth'
+import { useAuthReader } from '../services/useAuthReader'
 
 import { Footer } from './footer'
 
 export const AppLayout: FunctionComponent = props => {
   const { children } = props
+
+  const { asPath, push } = useRouter()
+  const user = useAuth()
+
+  const { dispatch } = useStoreon('user')
+
+  useAuthReader()
+
+  useEffect(() => {
+    dispatch('user/auth', user)
+  }, [user])
+
+  useEffect(() => {
+    if (
+      ['/', '/register', '/forgot'].includes(asPath) &&
+      user !== undefined &&
+      user !== null
+    ) {
+      push('/dashboard')
+    }
+  }, [asPath, user])
 
   return (
     <Fragment>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,3 @@
-import { useEffect } from 'react'
-
 import { NextPage } from 'next'
 import { AppProps } from 'next/app'
 import { useRouter } from 'next/router'
@@ -7,38 +5,16 @@ import { useRouter } from 'next/router'
 import { Context } from '../context/storeon'
 import { HeadTitle } from '../core/components/headTitle'
 import { AppLayout } from '../app/components/layout'
-import { useStoreon } from '../context/storeon'
 
 import { AuthWrapper } from '../app/components/authWrapper'
 import { DashboardWrapper } from '../app/components/dashboardWrapper'
-
-import { useAuthReader } from '../app/services/useAuthReader'
-import { useAuth } from '../core/services/useAuth'
 
 import '../styles/tailwind.css'
 
 const NextApp: NextPage<AppProps> = props => {
   const { Component, pageProps } = props
 
-  const { asPath, push } = useRouter()
-  const user = useAuth()
-  const { dispatch } = useStoreon('user')
-
-  useAuthReader()
-
-  useEffect(() => {
-    dispatch('user/auth', user)
-  }, [user])
-
-  useEffect(() => {
-    if (
-      ['/', '/register', '/forgot'].includes(asPath) &&
-      user !== undefined &&
-      user !== null
-    ) {
-      push('/dashboard')
-    }
-  }, [asPath, user])
+  const { asPath } = useRouter()
 
   return (
     <Context>
