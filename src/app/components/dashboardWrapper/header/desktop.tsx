d4cb753a14999dffe82c699f1b215752eacc46f1
Phumrapee Limpianchop <git@rayriffy.com>
2021-05-29 18:38:20 +0900

    chore: staged changes

--- src/app/components/dashboardWrapper/header/desktop.tsx
+++ src/app/components/dashboardWrapper/header/desktop.tsx
@@ -12,7 +12,7 @@ export const DesktopHeader = memo(() => {
         <div className="flex items-center flex-shrink-0 px-6">
           <img
             className="h-8 w-auto"
-            src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-gray-700-text.svg"
+            src="https://tailwindui.com/img/logos/workflow-logo-purple-500-mark-gray-700-text.svg"
             alt="Workflow"
           />
         </div>
