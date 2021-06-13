a57072d802bf01d3819433c47a2a8bcbda3c5b61
Phumrapee Limpianchop <git@rayriffy.com>
2021-06-13 18:50:04 +0900

    feat: refund api

---
+++ src/pages/api/cancel.ts
@@ -0,0 +1,85 @@
+import { NextApiHandler } from 'next'
+
+import { getCalculatedPrice } from '../../core/services/getCalculatedPrice'
+
+import firebase from 'firebase-admin'
+import { initializeFirebase } from '../../modules/api/services/initializeFirebase'
+import { getUserAndFilterAuth } from '../../modules/api/services/getUserAndFilterAuth'
+
+import { Arcade } from '../../core/@types/firebase/Arcade'
+import { Transaction } from '../../core/@types/firebase/Transaction'
+import { FaSmileBeam } from 'react-icons/fa'
+
+const api: NextApiHandler = async (req, res) => {
+  if (req.method.toLowerCase() === 'post') {
+    try {
+      initializeFirebase()
+
+      const userData = await getUserAndFilterAuth(
+        req.headers.authorization,
+        [],
+        true
+      )
+
+      const {
+        auth: { uid },
+        metadata: { balance },
+      } = userData
+      const { transactionId } = req.body
+
+      // todo: cancel transaction
+
+      // get transaction
+      const transactionRef = firebase
+        .firestore()
+        .collection('transaction')
+        .doc(transactionId)
+      const transactionDoc = await transactionRef.get()
+      const transactionData = transactionDoc.data() as Transaction
+
+      if (!transactionDoc.exists) {
+        return res.status(400).send({
+          success: false,
+          message: 'transaction not found',
+        })
+      } else if (transactionData.status !== 'pending') {
+        return res.status(400).send({
+          success: false,
+          message: 'not allowed at current state',
+        })
+      } else {
+        // cancel transaction
+        await transactionRef.update({
+          status: 'cancelled',
+          updatedAt: firebase.firestore.FieldValue.serverTimestamp(),
+        })
+        // refund money
+        await firebase
+          .firestore()
+          .collection('users')
+          .doc(uid)
+          .update({
+            balance: balance + transactionData.value,
+            updatedAt: firebase.firestore.FieldValue.serverTimestamp(),
+          })
+
+        return res.status(200).send({
+          success: true,
+          message: 'cancelled and refunded',
+        })
+      }
+    } catch (e) {
+      return res.status(403).send({
+        succcess: false,
+        message: 'unauthorized',
+      })
+    }
+  } else {
+    return res.status(405).send({
+      success: false,
+      message: 'invalid method',
+    })
+  }
+}
+
+export default api
