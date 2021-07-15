07491112f0f977871f4a61d0f737175d7c35cdf6
Phumrapee Limpianchop <git@rayriffy.com>
2021-07-15 16:19:34 +0000

    refactor: change firebase project

--- src/core/services/createFirebaseInstance.ts
+++ src/core/services/createFirebaseInstance.ts
@@ -3,13 +3,13 @@ import { initializeApp, getApp, getApps } from 'firebase/app'
 export const createFirebaseInstance = () => {
   const appInstance = !getApps().length
     ? initializeApp({
-        apiKey: 'AIzaSyCBJc4r9dsp0o1IP_RtnjbuFKbBPzU5gPs',
-        authDomain: 'mirai-ee873.firebaseapp.com',
-        projectId: 'mirai-ee873',
-        storageBucket: 'mirai-ee873.appspot.com',
-        messagingSenderId: '1034441026438',
-        appId: '1:1034441026438:web:3fb0574eb413fa7f2a1a07',
-        measurementId: 'G-GQ1KHNYT58',
+        apiKey: 'AIzaSyCQPV-Jt5-TnMVy-qaVpqjFAWsK7HyNWR0',
+        authDomain: 'mirai-da346.firebaseapp.com',
+        projectId: 'mirai-da346',
+        storageBucket: 'mirai-da346.appspot.com',
+        messagingSenderId: '721530926',
+        appId: '1:721530926:web:b6765b7614fb3b4046efbf',
+        measurementId: 'G-VK0WGTLSDK',
       })
     : getApp()
 
