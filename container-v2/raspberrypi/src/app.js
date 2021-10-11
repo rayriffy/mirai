4210df8b164b6576d110f0228eec0e293a25b61e
Phumrapee Limpianchop <git@rayriffy.com>
2021-10-11 19:36:17 +0900

    chore: staged changes

---
+++ container-v2/raspberrypi/src/app.js
@@ -0,0 +1,111 @@
+const { Server } = require('socket.io')
+const firebase = require('firebase-admin')
+const { networkInterfaces } = require('os')
+
+const serviceAccount = require('./mirai.json')
+
+const { STORE_ID } = process.env
+
+const wait = duration => new Promise(res => setTimeout(res, duration))
+
+// initialize socket.io server
+const io = new Server(11451, {
+  path: '/mirai-tx',
+  pingInterval: 10000,
+  pingTimeout: 5000,
+  cookie: false,
+  origins: '*:*',
+})
+
+// initialize firebase
+firebase.initializeApp({
+  credential: firebase.credential.cert(serviceAccount),
+})
+;(async () => {
+  console.log('[system]: booting')
+  await wait(50000000)
+
+  // list all ipv4
+  const allPossibleIps = Object.entries(networkInterfaces())
+    .filter(([key, val]) => val.some(({ family }) => family === 'IPv4'))
+    .map(([key, val]) =>
+      val
+        .filter(({ family }) => family === 'IPv4')
+        .map(({ address }) => address)
+    )
+    .flat()
+  console.log('[system]: all possible ip addresses')
+  allPossibleIps.map(o => console.log(`[system]: ${o}`))
+
+  // start listen for incoming transaction
+  console.log(`[system]: listening for transaction in store ${STORE_ID}`)
+  firebase
+    .firestore()
+    .collection('transactions')
+    .where('type', '!=', 'payment')
+    .where('storeId', '==', STORE_ID)
+    .where('status', '==', 'pending')
+    .orderBy('createdAt', 'desc')
+    .onSnapshot(snapshot => {
+      snapshot.docChanges().forEach(change => {
+        if (change.type === 'added') {
+          // shorten payload
+          processTransaction(change.doc)
+        }
+      })
+    })
+
+  // function to process payload
+  const processTransaction = async transaction => {
+    // update transaction status to processd
+    await firebase
+      .firestore()
+      .collection('transactions')
+      .doc(transaction.id)
+      .update({
+        status: 'processing',
+        updatedAt: firebase.firestore.FieldValue.serverTimestamp(),
+      })
+
+    // transmit the data
+    const transactionData = transaction.data()
+    const expectedReply = 3 * 1000 + transactionData.token * 100
+
+    // emit the data to the arcade
+    const eventName = transactionData.arcadeId
+    const payload = {
+      transactionId: transaction.id,
+      token: transactionData.token,
+    }
+
+    console.log(
+      `[system]: sending transaction ${transaction.id} to process (${transactionData.arcadeId}::${transactionData.token})`
+    )
+
+    io.emit(eventName, payload)
+  }
+
+  io.on('connection', socket => {
+    // listen for incoming transaction reply
+    socket.on('tx-reply', async transactionId => {
+      console.log(`[system]: putting ${transactionId} to success`)
+      // update transaction status to success
+      const transaction = await firebase
+        .firestore()
+        .collection('transactions')
+        .doc(transactionId)
+        .get()
+
+      if (transaction.data().status === 'processing') {
+        await firebase
+          .firestore()
+          .collection('transactions')
+          .doc(transactionId)
+          .update({
+            status: 'success',
+            updatedAt: firebase.firestore.FieldValue.serverTimestamp(),
+          })
+      }
+    })
+  })
+})()
