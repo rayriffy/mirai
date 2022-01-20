87f4ab2fff4af447b1c05c26a4ac3da4d7502edd
Phumrapee Limpianchop <git@rayriffy.com>
2022-01-20 20:25:00 +0900

    chore: staged changes

--- web/src/app/services/useUserMetadata.ts
+++ web/src/app/services/useUserMetadata.ts
@@ -1,14 +1,17 @@
 import { useEffect, useState, useMemo } from 'react'
 
+import { useRouter } from 'next/router'
+import { useStoreon } from '../../context/storeon'
+
 import { onSnapshot, collection, doc, getFirestore } from 'firebase/firestore'
 import { createFirebaseInstance } from '../../core/services/createFirebaseInstance'
-import { useStoreon } from '../../context/storeon'
 
 import { User } from '../../core/@types/firebase/User'
 
 export const useUserMetadata = (uid: string) => {
   const [metadata, setMetadata] = useState<User | null>(undefined)
 
+  const router = useRouter()
   const { dispatch } = useStoreon('user')
 
   const isUIDVaid = useMemo(
@@ -33,6 +36,7 @@ export const useUserMetadata = (uid: string) => {
 
               if (data === undefined) {
                 setMetadata(null)
+                router.push('/onboarding')
               } else {
                 setMetadata(data)
               }
