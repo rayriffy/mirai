084c9ffcc84de2fae91459f95a4657652df5ebee
Phumrapee Limpianchop <git@rayriffy.com>
2022-03-01 22:48:22 +0900

    chore: layout

--- web/src/pages/dashboard/index.tsx
+++ web/src/pages/dashboard/index.tsx
@@ -29,20 +29,20 @@ const Page: NextPage<Props> = props => {
 
   return (
     <Fragment>
-      <div className="border-b border-gray-200 px-4 py-4 sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8">
+      <div className="border-b border-gray-200 px-4 py-4 md:flex sm:items-center sm:justify-between sm:px-6 lg:px-8">
         <div className="flex-1 min-w-0">
           <h1 className="text-lg font-medium leading-6 text-gray-900 sm:truncate">
             {locale('home')}
           </h1>
         </div>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5">
-        <div className="col-span-1 px-4 mt-6 sm:px-6 lg:px-8 space-y-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+        <div className="col-span-1 sm:col-span-2 md:col-span-1 px-4 mt-6 sm:px-6 lg:px-8 space-y-4">
           <QRProfile />
           <Balance />
         </div>
 
-        <div className="col-span-1 md:col-span-1 lg:col-span-3 xl:col-span-4 px-4 mt-6 sm:px-6 lg:px-8">
+        <div className="col-span-1 md:col-span-2 lg:col-span-3 xl:col-span-4 px-4 mt-6 sm:px-6 lg:px-8">
           <Alpha endTestDate={dayjs(endTestingDate)} />
           <FavoriteArcades />
         </div>
