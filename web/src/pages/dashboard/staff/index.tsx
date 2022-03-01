3b9ca0fc279f5137eca83452f20189777fbbabd9
Phumrapee Limpianchop <git@rayriffy.com>
2022-03-02 05:35:49 +0900

    chore: staged changes

--- web/src/pages/dashboard/staff/index.tsx
+++ web/src/pages/dashboard/staff/index.tsx
@@ -1,4 +1,5 @@
 import { NextPage } from 'next'
+import Link from 'next/link'
 
 import { useStoreon } from '../../../context/storeon'
 import { useLocale } from '../../../core/services/useLocale'
@@ -24,21 +25,21 @@ const Page: NextPage = () => {
         <p className="flex text-sm text-gray-600">
           <span>{user.metadata.staffStoreName}</span>
           <span className="mx-2">·</span>
-          <span className="flex items-center">{user.metadata.staffStoreId}</span>
+          <span className="flex items-center">
+            {user.metadata.staffStoreId}
+          </span>
         </p>
       </div>
       <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-6">
         {menus.map(menu => (
-          <a
-            className="col-span-1 rounded-lg p-6 space-y-2 transition border border-gray-200 hover:border-blue-500 hover:text-blue-500 bg-white"
-            href={menu.link}
-            key={`admin-menu-${menu.id}`}
-          >
-            <h1 className="text-2xl font-bold inline-flex items-center">
-              <menu.icon className="w-6 h-6 mr-2" /> {menu.title}
-            </h1>
-            <p>{menu.description}</p>
-          </a>
+          <Link href={menu.link} key={`admin-menu-${menu.id}`}>
+            <a className="col-span-1 rounded-lg p-6 space-y-2 transition border border-gray-200 hover:border-blue-500 hover:text-blue-500 bg-white">
+              <h1 className="text-2xl font-bold inline-flex items-center">
+                <menu.icon className="w-6 h-6 mr-2" /> {menu.title}
+              </h1>
+              <p>{menu.description}</p>
+            </a>
+          </Link>
         ))}
       </div>
     </div>
