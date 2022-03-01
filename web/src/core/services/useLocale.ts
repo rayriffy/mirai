3bec12ab31380411700522e40332a512a622a255
Phumrapee Limpianchop <git@rayriffy.com>
2022-03-02 07:16:20 +0900

    chore: sync useLocale

--- web/src/core/services/useLocale.ts
+++ web/src/core/services/useLocale.ts
@@ -4,18 +4,32 @@ import { useRouter } from 'next/router'
 
 type Language = 'en' | 'th'
 
+// type Input = Partial<
+//   Record<
+//     Language,
+//     {
+//       [key: string]: ReactText
+//     }
+//   >
+// >
+
 type Input = Record<
   Language,
   {
-    [key: string]: ReactText
+    [key: string]: string
   }
 >
 
 export const useLocale = (input: Input) => {
   const { locale: detectedLocale } = useRouter()
 
-  const locale = (key: keyof Input['en']): string =>
-    input[window.detectedLocale][key] ?? input['en'][key] ?? `#${key}#`
+  const locale = (key: keyof Input['en']): string => {
+    if (input[window.detectedLocale] === undefined) {
+      return input['en'][key] ?? `#${key}#`
+    }
+
+    return input[window.detectedLocale][key] ?? input['en'][key] ?? `#${key}#`
+  }
 
   return { locale, detectedLocale }
 }
