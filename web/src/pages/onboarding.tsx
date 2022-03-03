a7a0c2accd04c7216f53489a211ce0e63d3d2d10
Phumrapee Limpianchop <git@rayriffy.com>
2022-03-03 20:41:39 +0900

    chore: staged changes

--- web/src/pages/onboarding.tsx
+++ web/src/pages/onboarding.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react'
+import { Fragment, useCallback, useEffect, useState } from 'react'
 
 import { NextPage } from 'next'
 
@@ -12,6 +12,8 @@ import { Step3 } from '../modules/auth/onboarding/components/step3'
 import { Input } from '../modules/auth/onboarding/@types/Input'
 import { useStoreon } from '../context/storeon'
 import { useRouter } from 'next/router'
+import { useNetworkAvailability } from 'web-api-hooks'
+import { StatusOfflineIcon } from '@heroicons/react/outline'
 
 const Page: NextPage = () => {
   const { locale } = useLocale({
@@ -33,32 +35,51 @@ const Page: NextPage = () => {
     displayName: '',
     preferredStore: '',
   })
+  const isOnline = useNetworkAvailability()
+
+  const router = useRouter()
+  const { user } = useStoreon('user')
+  useEffect(() => {
+    if (typeof user?.metadata?.displayName === 'string') {
+      router.push('/dashboard')
+    }
+  }, [user?.metadata?.displayName])
 
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
-      <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <h2 className="mt-6 text-center text-3xl md:text-4xl font-extrabold text-gray-900">
-          {locale('title')}!
-        </h2>
-        <p className="text-center text-sm text-gray-600 max-w">
-          {locale('subtitle')}
-        </p>
-      </div>
-      <div className="py-4">
-        <Step current={step} total={3} />
-      </div>
-
-      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-        <div className="bg-white shadow sm:rounded-lg">
-          {step === 1 ? (
-            <Step1 {...{ input, setInput, onNext }} />
-          ) : step === 2 ? (
-            <Step2 {...{ input, setInput, onPrev, onNext }} />
-          ) : (
-            <Step3 {...{ input }} />
-          )}
+      {!isOnline ? (
+        <div className="mx-auto text-gray-500 text-center">
+          <StatusOfflineIcon className="w-12 h-12 mx-auto" />
+          <h1 className="text-lg font-semibold">No internet connection</h1>
+          <p>Please connect to the internet and restart the app</p>
         </div>
-      </div>
+      ) : (
+        <Fragment>
+          <div className="sm:mx-auto sm:w-full sm:max-w-md">
+            <h2 className="mt-6 text-center text-3xl md:text-4xl font-extrabold text-gray-900">
+              {locale('title')}!
+            </h2>
+            <p className="text-center text-sm text-gray-600 max-w">
+              {locale('subtitle')}
+            </p>
+          </div>
+          <div className="py-4">
+            <Step current={step} total={3} />
+          </div>
+
+          <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+            <div className="bg-white shadow sm:rounded-lg">
+              {step === 1 ? (
+                <Step1 {...{ input, setInput, onNext }} />
+              ) : step === 2 ? (
+                <Step2 {...{ input, setInput, onPrev, onNext }} />
+              ) : (
+                <Step3 {...{ input }} />
+              )}
+            </div>
+          </div>
+        </Fragment>
+      )}
     </div>
   )
 }
