d4604795605195ed752acafd3ac618297f5f5444
Phumrapee Limpianchop <git@rayriffy.com>
2022-03-05 07:28:41 +0900

    feat: app check

--- web/src/core/services/createFirebaseInstance.ts
+++ web/src/core/services/createFirebaseInstance.ts
@@ -1,17 +1,25 @@
 import { initializeApp, getApp, getApps } from 'firebase/app'
+import { initializeAppCheck, ReCaptchaV3Provider } from 'firebase/app-check'
 
 export const createFirebaseInstance = () => {
-  const appInstance = !getApps().length
-    ? initializeApp({
-        apiKey: 'AIzaSyCQPV-Jt5-TnMVy-qaVpqjFAWsK7HyNWR0',
-        authDomain: 'mirai-da346.firebaseapp.com',
-        projectId: 'mirai-da346',
-        storageBucket: 'mirai-da346.appspot.com',
-        messagingSenderId: '721530926',
-        appId: '1:721530926:web:b6765b7614fb3b4046efbf',
-        measurementId: 'G-VK0WGTLSDK',
-      })
-    : getApp()
+  if (!getApps().length) {
+    const app = initializeApp({
+      apiKey: 'AIzaSyCQPV-Jt5-TnMVy-qaVpqjFAWsK7HyNWR0',
+      authDomain: 'mirai-da346.firebaseapp.com',
+      projectId: 'mirai-da346',
+      storageBucket: 'mirai-da346.appspot.com',
+      messagingSenderId: '721530926',
+      appId: '1:721530926:web:b6765b7614fb3b4046efbf',
+      measurementId: 'G-VK0WGTLSDK',
+    })
 
-  return appInstance
+    const appCheck = initializeAppCheck(app, {
+      provider: new ReCaptchaV3Provider('6LdwJrUeAAAAAG_aFR4xoJMHK-MY_KVkJE_HiVpy'),
+      isTokenAutoRefreshEnabled: true
+    })
+
+    return app
+  } else {
+    return getApp()
+  }
 }
