9821a2ccc43b4eba5dac7361751f45d9616e6f7b
Phumrapee Limpianchop <git@rayriffy.com>
2022-03-05 16:37:13 +0900

    chore: staged changes

--- web/src/pages/fastTrackArcade.tsx
+++ web/src/pages/fastTrackArcade.tsx
@@ -1,7 +1,27 @@
 import { NextPage } from 'next'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+
+import { useStoreon } from '../context/storeon'
+import { Spinner } from '../core/components/spinner'
 
 const Page: NextPage = props => {
-  return <></>
+  const { user: { metadata } } = useStoreon('user')
+  const router = useRouter()
+
+  useEffect(() => {
+    if (metadata?.preferredStore !== null && metadata?.preferredStore !== undefined) {
+      router.push(`/dashboard/arcades/${metadata?.preferredStore}`)
+    }
+  }, [metadata?.preferredStore])
+
+  return (
+    <div
+      className={'min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8 items-center'}
+    >
+      <Spinner />
+    </div>
+  )
 }
 
 export default Page
